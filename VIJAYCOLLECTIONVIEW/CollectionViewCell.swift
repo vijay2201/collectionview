5edc8ac83277cd2e96d11448e50841e20ed0a22a
COE-008 <coe-008@coe-008s-imac.local>
2019-12-26 13:50:13 +0530

    vijayprajapati

---
+++ VIJAYCOLLECTIONVIEW/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  VIJAYCOLLECTIONVIEW
+//
+//  Created by COE-008 on 23/12/19.
+//  Copyright © 2019 COE-008. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+
+    @IBOutlet weak var lbl: UILabel!
+}
--- VIJAYCOLLECTIONVIEW/ViewController.swift
+++ VIJAYCOLLECTIONVIEW/ViewController.swift
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+   
+    
+    
+    var arr = [["Image":#imageLiteral(resourceName: "img2") , "Name":"Virat1"],
+              ["Image":#imageLiteral(resourceName: "img1") , "Name":"Virat2"],
+              ["Image":#imageLiteral(resourceName: "img3") ,"Name":"Virat3"]]
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as!CollectionViewCell
+        cell.img.image = arr[indexPath.row]["Image"] as? UIImage
+        cell.lbl.text = arr[indexPath.row]["Name"] as? String
+        
+        return cell
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
